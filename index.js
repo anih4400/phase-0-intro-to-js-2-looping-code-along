89d264d1bf6769aaf3662365f0c07990b082da07
AnishPatel <anish4400@gmail.com>
2022-11-26 19:26:02 -0500

    commit tree 6bcc070f25d685f491506384a45a3d6a5bd40640

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+const gifts = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards() {
+  let thankyou = [];
+  let i = 0;
+    while (i< gifts.length){
+    thankyou.push(`Thank you, ${gifts[i]}, for the wonderful surprise gift!`);
+    i++;
+  }
+return thankyou;
+}
+
+function countDown(n) {
+    while (n>=0){
+    console.log(n);
+    n--;
+  }
+}
